69cfbc13f65d91579b9768aaa7f42e7337b3c3b9
Brianlucca <brianlucca2002@gmail.com>
2023-10-04 16:45:52 -0300

    Criando lista

---
+++ Assents/Js/lista.js
@@ -0,0 +1,66 @@
+const input1 = document.getElementById("nome1");
+const btn = document.querySelector("button");
+const div = document.getElementById("resultado");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdAz80uSFEYSVRgOzoV-p7IGMcnIkc7Og",
+  authDomain: "musicas-34b84.firebaseapp.com",
+  databaseURL: "https://musicas-34b84-default-rtdb.firebaseio.com",
+  projectId: "musicas-34b84",
+  storageBucket: "musicas-34b84.appspot.com",
+  messagingSenderId: "980655490969",
+  appId: "1:980655490969:web:e6f75df4f533c1c0a0b508",
+  measurementId: "G-KP78DQYW4D",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+const bancodedadosLocal =
+  JSON.parse(localStorage.getItem("bancodedados")) || [];
+
+function exibirLista() {
+  div.innerHTML = ""; // Limpe o conteúdo para evitar duplicatas
+
+  bancodedadosLocal.map(({nome1}, index) => {
+    div.innerHTML += `<div>
+                        <input class="inputResultado" type="checkbox">${nome1}</h3>
+                        <button class="buttonResultado" onclick="excluirLista(${index})">Apagar</button>
+                      </div>`;
+  });
+}
+
+exibirLista(); // Chame a função para exibir as anotações iniciais
+
+btn.addEventListener("click", () => {
+  const nome1 = input1.value;
+
+  const novaLista = {
+    nome1,
+  };
+
+  bancodedadosLocal.push(novaLista);
+
+  // Envie os dados para o Firebase
+  salvarNoFirebase(novaLista);
+
+  localStorage.setItem("bancodedados", JSON.stringify(bancodedadosLocal));
+  const mensagemDiv = document.getElementById("mensagemResultado");
+  mensagemDiv.textContent =
+    "Sua lista foi salva. Por favor, atualize a página.";
+});
+
+function salvarNoFirebase(novaLista) {
+  const firebaseRef = database.ref("listas");
+  firebaseRef.push(novaLista);
+}
+
+function excluirLista(index) {
+  if (confirm("Tem certeza de que deseja excluir esta lista?")) {
+    bancodedadosLocal.splice(index, 1);
+    localStorage.setItem("bancodedados", JSON.stringify(bancodedadosLocal));
+
+    exibirLista(); // Atualize a exibição após a exclusão
+  }
+}
